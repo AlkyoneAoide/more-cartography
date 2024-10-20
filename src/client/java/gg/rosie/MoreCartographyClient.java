7294cda018a3e521495f2b236be23f8cd773371e
Rose Hart <rose@rosie.gg>
2024-10-19 18:07:11 -0600

    init with template

---
+++ src/client/java/gg/rosie/MoreCartographyClient.java
@@ -0,0 +1,10 @@
+package gg.rosie;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MoreCartographyClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
